98daf4ab87af6ee82116cb95c4a875ab1e446e16
Kaushal Vishwas <129539928+VISHIV05@users.noreply.github.com>
2025-06-25 01:28:14 +0530

    Update RedVsBlue.cpp
    This is cp problem solution

--- RedVsBlue.cpp
+++ RedVsBlue.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void solve(){
   int n,r,b;
   cin>>n>>r>>b;
-
+//pigeonhole principle
   int p=(r/(b+1));
   int q=(r%(b+1));
   for(int i=0;i<q;i++) cout<<string(p+1,'R')<<'B';
